9096530bff447fb59de7876cf2f6df2167706a93
JayramMardi <82032414+JayramMardi@users.noreply.github.com>
2021-07-29 22:46:58 +0530

    Update pythonforbegginer.py

--- pythonforbegginer.py
+++ pythonforbegginer.py
@@ -2,3 +2,24 @@
 
 python is easy to use and less development time, high level  language it means don't very about the memory allocated or crash your proggramer code portablity first class it installed on windows/man /linux and instllation is very quick .
 
+
+# Modules PiP And  Comments 
+
+module are the code which is written  by  somone else and use it for coding purpose for everyone by downloading and write in the terminal pip install module name and hit enter . 
+
+
+# 01 hello.py
+
+print("hello world")
+
+# what are pip
+
+pip are basically python pacakage installer and use it for download modules which are  pre installed with the python interpreter . 
+
+# what are comments 
+
+comments are basically readible but not execute cause this not a valid syntax of python programming language . comments can be written in sinle line comments and multiline comments , # for single and ''' for multiline and both written under the comments
+
+'''
+hello world
+'''
