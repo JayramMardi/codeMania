1b0901b743487658fd81f7d9fbf0476ce5627efa
JayramMardi <82032414+JayramMardi@users.noreply.github.com>
2021-07-29 22:44:08 +0530

    Create pythonforbegginer.py

---
+++ pythonforbegginer.py
@@ -0,0 +1,4 @@
+# What Is Programming
+
+python is easy to use and less development time, high level  language it means don't very about the memory allocated or crash your proggramer code portablity first class it installed on windows/man /linux and instllation is very quick .
+
